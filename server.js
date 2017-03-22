d4804cc93b97ecfd9edf8a05c9ce6cdb38b47c63
ajayshankarr1 <ajayshankar.ramachandran@leanswift.com>
2017-03-22 10:50:54 +0530

    eLink build.json: added eLink-CE
    git checkout, maven build working, properties parser in progress

--- server.js
+++ server.js
@@ -4,21 +4,25 @@ var app = express();
 var fs = require('fs');
 var nodegit = require('nodegit');
 var parseString = require('xml2js').parseString;
-var maven = require('maven-deploy');
+var maven = require('maven');
+var Q = require('q');
+var prop = require('properties-parser');
 var localPath = path.join.bind(path,__dirname);
 var Checkout = nodegit.Checkout;
 var Tag = nodegit.Tag;
 var Reset = nodegit.Reset;
+var editor = prop.createEditor();
 
 app.get('/versions', function(req, res) {
 	fs.readFile(__dirname + "/eLink-build.json", "utf-8", function(err, data) {
 		var json = JSON.parse(data);
 		var versions = {
-				versions: []
+			versions: []
 		};
 		json.eLinkBuilds.forEach(function(item, index) {
 			versions.versions[index] = item.version;
 		});
+		console.log(path.dirname);
 		res.end(JSON.stringify(versions));
 	})
 });
@@ -27,7 +31,7 @@ app.get('/:version/customizables', function(req, res) {
 	fs.readFile(__dirname + "/eLink-build.json", "utf-8", function(err, data) {
 		var json = JSON.parse(data);
 		var customizables = {
-				customizables: []
+			customizables: []
 		};
 		json.eLinkBuilds.forEach(function(item, index) {
 			if(item.version == req.params.version){
@@ -55,115 +59,176 @@ app.get('/:version/download', function(req, res) {
 
 
 		var opts = {
-				fetchOpts: {
-					callbacks: {
-						certificateCheck: function() {
-							return 1;
-						},
-						credentials: function(url, userName) {
-							return nodegit.Cred.sshKeyNew(userName, sshPublicKeyPath, sshPrivateKeyPath, "");
-						}
+			fetchOpts: {
+				callbacks: {
+					certificateCheck: function() {
+						return 1;
+					},
+					credentials: function(url, userName) {
+						return nodegit.Cred.sshKeyNew(userName, sshPublicKeyPath, sshPrivateKeyPath, "");
 					}
 				}
-		};
-		var modules = {
-				modules: []
-		};
-		var customizables = {
-			customizables: []
+			}
 		};
 		var repo;
 		var xml;
-		json.eLinkBuilds.forEach(function(item, index) {
-			if(item.version == req.params.version) {
-				item.parameters.forEach(function(item, index) {
-					customizables.customizables[index] = {};
-					customizables.customizables.key = item.key;
-					customizables.customizables.fileName = item.fileName;
-					customizables.customizables.location = item.location;
-				});
-				//XML EDITOR
-				// fs.readFile(__dirname + "/eLink-mp-context.xml", "utf-8", function(err, data){
-				// 	if(err){
-				// 		console.log(err);
-				// 	}
-				// 	var jsonforXML;
-				// 	parseString(data, function(err, result) {
-				// 		if (err) {
-				// 			console.log(err);
-				// 		}
-				// 		jsonforXML = result;
-				// 	//
-				// 	});
-				// 	jsonforXML.beans.bean.forEach(function(item, index) {
-				// 		if (item.$.id == "h2WebServer") {
-				// 			jsonforXML.beans.bean.splice(1, index);
-				// 		}
-				// 	});
-				// 	console.log(JSON.stringify(jsonforXML));
-				// });
-				//GIT
-				item.modules.forEach(function(item, index){
-					modules.modules[index] = {};
-					modules.modules.repository = item.repository;
-					modules.modules.branch = item.branch;
-				repo = nodegit.Clone(git_url+item.repository.toString()+".git", clonePath+item.repository.toString(), opts).then(function(repo) {
-					console.log("Repository cloning done");
-						  Tag.list(repo).then(function(array){
-						 							return repo.getReferenceCommit(item.branch);
-												}).then(function(commit) {
-						 							//repo.getCommit(nodegit.Oid.fromString(commitSha.id().toString())).then(function(commit){
-						 								console.log("get commit done");
-						 								Checkout.tree(repo, commit, { checkoutStrategy: Checkout.SAFE}).then(function(){
-						 													 		repo.setHeadDetached(commit, repo.defaultSignature, "Checkout: HEAD" + commit);
-																				}).then(function(){
-																					Reset.reset(repo,commit,Reset.TYPE.HARD,{checkoutStrategy: Checkout.SAFE},item.branch);
-																			 console.log("Checkout done");
-						 										 }).catch(function(err){
-						 											 console.log(err.message, "after checkout"+item.repository);
-						 										 });
-						 							}).catch(function(err){
-						 								console.log(err.message, "after getting commit"+item.repository );
-						 							});
-											// 	}).catch(function(err){
-											// 			console.log(err.message, "after getting reference"+item.repository);
-											// });
-				}).catch(function(err) {
-					console.log(err.message);
-				});
+
+		var deferred = Q.defer();
+
+		var gitCheckoutfunction = function(){
+			json.eLinkBuilds.forEach(function(item, index){
+				if(item.version == req.params.version){
+					item.modules.forEach(function(item, index){
+						repo = nodegit.Clone(git_url+item.repository+".git", clonePath+item.repository, opts).then(function(repo) {
+							console.log("Repository cloning done");
+							Tag.list(repo).then(function(array){
+								return repo.getReferenceCommit(item.branch);
+							}).then(function(commit) {
+								console.log("get commit done");
+								Checkout.tree(repo, commit, { checkoutStrategy: Checkout.SAFE}).then(function(){
+									repo.setHeadDetached(commit, repo.defaultSignature, "Checkout: HEAD" + commit);
+								}).then(function(){
+									Reset.reset(repo,commit,Reset.TYPE.HARD,{checkoutStrategy: Checkout.SAFE},item.branch);
+									console.log("Checkout done");
+								}).catch(function(err){
+									console.log(err.message, "after checkout"+item.repository);
+								});
+							}).catch(function(err){
+								console.log(err.message, "after getting commit"+item.repository );
+							});
+						}).catch(function(err) {
+							console.log(err.message);
+						}).done(function(){
+							console.log("finished");
+						});
+					});
+				}
 			});
-			//MAVEN build
-			// var config = {
-			// 	"groupId" 		: "com.leanswift",
-			// 	"artifactId"	: "eLink-parent",
-			// 	"buildDir"		: clonePath.toString(),
-			// 	"finalName"		: "eLink-parent",
-			// 	"type"				: "jar",
-			// 	"fileEncoding": "utf-8",
-			// 	"pomFile"			: "pom.xml",
-			// 	"repositories": [
-			// 		{
-			// 			"id" : "thirdparty.leanswift",
-			// 			"url": "http://maven.leanswift.net:3434/artifactory/thirdparty.leanswift"
-			// 		},
-			// 		{
-			// 			"id" : "leanswift.releases",
-			// 			"url": "http://maven.leanswift.net:3434/artifactory/leanswift.releases"
-			// 		},
-			// 		{
-			// 			"id" : "leanswift.snapshots",
-			// 			"url": "http://maven.leanswift.net:3434/artifactory/leanswift.snapshots"
-			// 		}
-			// 	]
-			// };
-			// maven.config(config);
-			// maven.install();
-			}
-		});
+		};
+
+		var xmlEditorFunction = function() {
+			json.eLinkBuilds.forEach(function(item, index){
+				if(item.version == req.params.version){
+					item.parameters.forEach(function(item, index) {
+						var properties = prop.read(clonePath + item.location + item.fileName);
+						editor.set(item.key, item.value);
+						console.log(properties.toString());
+					});
+				}
+			});
+		}
+
+		var mavenBuildfunction = function() {
+			json.eLinkBuilds.forEach(function(item, index){
+				if(item.version == req.params.version){
+					item.modules.forEach(function(item, index){
+						//deferred.resolve(function(){
+							var mvn = maven.create({
+							cwd: clonePath + item.repository
+						});
+						mvn.execute(['clean', 'install']).then(function(result){
+							deferred.resolve(result);
+							console.log("build success");
+						}).catch(function(err){
+							console.log(err);
+						});
+					//});
+					return deferred.promise;
+					});
+				}
+			});
+		}
+
+		function promiseForGitfunction() {
+			deferred.resolve(gitCheckoutfunction());
+
+			return deferred.promise;
+		}
+		promiseForGitfunction().then(function(result){
+			deferred.resolve(xmlEditorFunction());
+
+			return deferred.promise;
+		}).then(function(){
+			mavenBuildfunction();
+		})
+
+
+
+		//	json.eLinkBuilds.forEach(function(item, index) {
+		//			if(item.version == req.params.version) {
+		// 	item.modules.forEach(function(item, index){
+		// 		modules.modules[index] = {};
+		// 		modules.modules.repository = item.repository;
+		// 		modules.modules.branch = item.branch;
+		// 		repo = nodegit.Clone(git_url+item.repository+".git", clonePath+item.repository, opts).then(function(repo) {
+		// 			console.log("Repository cloning done");
+		// 			Tag.list(repo).then(function(array){
+		// 				return repo.getReferenceCommit(item.branch);
+		// 			}).then(function(commit) {
+		// 				console.log("get commit done");
+		// 				Checkout.tree(repo, commit, { checkoutStrategy: Checkout.SAFE}).then(function(){
+		// 					repo.setHeadDetached(commit, repo.defaultSignature, "Checkout: HEAD" + commit);
+		// 				}).then(function(){
+		// 					Reset.reset(repo,commit,Reset.TYPE.HARD,{checkoutStrategy: Checkout.SAFE},item.branch);
+		// 					console.log("Checkout done");
+		// 				}).catch(function(err){
+		// 					console.log(err.message, "after checkout"+item.repository);
+		// 				});
+		// 			}).catch(function(err){
+		// 				console.log(err.message, "after getting commit"+item.repository );
+		// 			});
+		// 		}).catch(function(err) {
+		// 			console.log(err.message);
+		// 		}).done(function(){
+		// 			console.log("finished");
+		// 	});
+		// });
+
+		// 	item.parameters.forEach(function(item, index) {
+		// 		customizables.customizables[index] = {};
+		// 		customizables.customizables.key = item.key;
+		// 		customizables.customizables.value = item.value;
+		// 		customizables.customizables.fileName = item.fileName;
+		// 		customizables.customizables.location = item.location;
+		// 	//XML EDITOR
+		// 	// fs.readFile(clonePath + item.location + item.fileName, "utf-8", function(err, data){
+		// 	// 	if(err){
+		// 	// 		console.log(err);
+		// 	// 	}
+		// 	// 	var jsonforXML;
+		// 	// 	parseString(data, function(err, result) {
+		// 	// 		if (err) {
+		// 	// 			console.log(err);
+		// 	// 		}
+		// 	// 		jsonforXML = result;
+		// 	// 	//
+		// 	// 	});
+		// 	// 	// jsonforXML.beans.bean.forEach(function(item, index) {
+		// 	// 	// 	if (item.$.id == "h2WebServer") {
+		// 	// 	// 		jsonforXML.beans.bean.splice(1, index);
+		// 	// 	// 	}
+		// 	// 	// });
+		// 	// 	console.log(JSON.stringify(jsonforXML));
+		// 	// });
+		// 	//properties EDITOR
+		// 	var editor = prop.createEditor(clonePath + item.location + item.fileName);
+		// 	console.log(editor.toString());
+		// });
+		//	}
+		//});
+		//MAVEN BUILD
+		// var mvn = maven.create({
+		// 	cwd: clonePath + "eLink-CE"
+		// });
+		// mvn.execute(['clean', 'install']).then(function(){
+		// 	console.log("build success");
+		// }).catch(function(err){
+		// 	console.log(err);
+		// });
 	})
 });
 
-var server = app.listen(5858, "127.0.0.1", function() {
+var server = app.listen(8888, "127.0.0.1", function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
